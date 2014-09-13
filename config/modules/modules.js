18217f77eb0665629daa054023407236c6c902d2
roman <shmoop207@gmail.com>
2014-09-13 12:36:26 +0300

    git image

--- config/modules/modules.js
+++ config/modules/modules.js
@@ -2,5 +2,5 @@ var loggerModule= require('./logger/logger'),
     mongooseModule= require('./mongoose/mongoose'),
     appolo = require('appolo-express');
 
-appolo.module.register(loggerModule());
-appolo.module.register(mongooseModule());
+appolo.use(loggerModule());
+appolo.use(mongooseModule());
